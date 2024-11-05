3470483e3a630d72a1f4aae02c952d4161f9622c
Aparecida Ferreira <108874543+cidaci2000@users.noreply.github.com>
2024-11-05 20:16:37 +0000

    nnnn

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -1,22 +1,18 @@
-const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
-
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json';
 
 async function vizualizarInformacoesGlobais() {
-
-    const respota = await fetch(url)
-
-    const dados = await respota.json()
-
+    const res = await fetch(url);
+    const dados = await res.json();
     //console.log(dados)
-
+    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `O mundo tem<span>${dados.total_pessoas_mundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${dadostotal_trabalhadores_mundo}</span>   bilhões estão empregadas e passam em média <span>${dados.tempo_medio_deslocamento_para_trabalho
-        }</span>  horas por dia trabalhando. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
+    paragrafo.innerHTML = `Com <span>${dados.total_pessoas_mundo}</span>  bilhões de pessoas no mundo, <span>${dados.total_trabalhadores_mundo}</span> bilhões são trabalhadores e  <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> bilhões estão empregadas e passam em média. Isso representa 38% e 67% da força de trabalho, respectivamente, com um tempo médio de deslocamento de  <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
 
 vizualizarInformacoesGlobais();
+
 
 
