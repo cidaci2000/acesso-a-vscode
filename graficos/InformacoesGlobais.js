f06137df4e6533555a4f5c5bffc22674c0bd05aa
Aparecida Ferreira <108874543+cidaci2000@users.noreply.github.com>
2024-11-05 19:32:38 +0000

    wwwww

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
+const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
 
 
 async function vizualizarInformacoesGlobais() {
@@ -12,8 +12,7 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `O mundo tem<span>${dados.total_pessoas_mundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${dadostotal_trabalhadores_mundo}</span>   bilhões estão empregadas e passam em média <span>${dados.tempo_medio_deslocamento_para_trabalho
-}</span>  horas por dia trabalhando.
-Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
+        }</span>  horas por dia trabalhando. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
